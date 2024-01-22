7216c8d8dec6bc668b35b58706b8a94cf4408743
ECU44 <91726557+ECU44@users.noreply.github.com>
2024-01-22 21:16:57 +0300

    fix

--- src/pages/top-five-customer.tsx
+++ src/pages/top-five-customer.tsx
@@ -1,51 +1,31 @@
-import React, { FC, useState, useEffect } from "react";
+import { FC, useState } from "react";
 import { useParams } from "react-router-dom";
 import CustomerNavbar from "../components/customer-navbar";
 import { Row, Col, Table } from "antd";
 import axios from "axios";
-
-interface Customer {
-  id: number;
-  name: string;
-  meTooCount: number;
-}
+import { useQuery } from "react-query";
 
 const TopFiveCustomer: FC = () => {
+      
   const { id } = useParams<{ id: string }>();
-  const [customers, setCustomers] = useState<Customer[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchDataCustomer = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get("http://localhost:3000/complaint/me-too-customer");
-        setCustomers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchDataCustomer = async () => {
+    const response = await axios.get("http://localhost:3000/complaint/me-too-customer");
+    console.log(response.data);
+    return response.data;
+  };
 
-    fetchDataCustomer();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error loading data</div>;
+  const { data: meTooData } = useQuery("meTooCountByCustomer", fetchDataCustomer);
 
   return (
-    <div>
-      <h2>Customer List</h2>
-      <ul>
-        {customers.map(customer => (
-          <li key={customer.id}>
-            {customer.name} - MeToo Count: {customer.meTooCount}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Row>
+        <Col span={24}>
+            <span style={{ fontSize: "2rem", fontWeight: "bold" }}>Top 5 Customers</span>
+            <span>
+            {meTooData }
+             </span>
+        </Col>
+    </Row>
   );
 };
 
