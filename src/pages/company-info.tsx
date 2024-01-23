47701be736dec1dd0d6662d3d691b266c1c3de2d
ECU44 <91726557+ECU44@users.noreply.github.com>
2024-01-23 17:03:44 +0300

    new fix

--- src/pages/company-info.tsx
+++ src/pages/company-info.tsx
@@ -3,22 +3,52 @@ import { FC } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import CompanyAgentNavbar from "../components/company-agent-navbar";
-import { Card, Col, Row } from "antd";
-import imagecompany from "../assets/company.png"
+import { Card, Col, Row, Table } from "antd";
+import imagecompany from "../assets/company.png";
 
 const CompanyInfo: FC = () => {
   const { id } = useParams<{ id: string }>();
   const getCompanyByAgentId = async () => {
     const response = await axios.get(
-        `http://localhost:3000/company-agent/company`,
-        { params:  { id: parseInt(id)}}
+      `http://localhost:3000/company-agent/company`,
+      { params: { id: parseInt(id) } }
     );
     return response.data;
   };
   const { data } = useQuery("company", getCompanyByAgentId);
   const company = data?.company[0];
-  console.log(company);
 
+  const getactivefivecompanyagent = async () => {
+    const response = await axios.get(
+      `http://localhost:3000/company/active-company-agents`,
+      { params: { id: parseInt(id) } }
+    );
+    return response.data;
+  };
+  const { data: datas } = useQuery("companyagents", getactivefivecompanyagent);
+  const companyagent = datas?.activeCompanyAgents;
+  console.log(companyagent);
+
+  const columns = [
+    {
+      title: "Comapny Agent Name",
+      dataIndex: "name",
+      key: "customerName",
+      sorter: (a: any, b: any) => (a.name > b.name ? 1 : -1),
+    },
+    {
+      title: "Comapny Agent ID",
+      dataIndex: "company_agent_id",
+      key: "customer_agent_id",
+      sorter: (a: any, b: any) => (a.name > b.name ? 1 : -1),
+    },
+    {
+      title: "Total Solutions Solved",
+      dataIndex: "total_solutions_solved",
+      key: "total_solutions_solved",
+      sorter: (a: any, b: any) => a.meTooCount - b.meTooCount,
+    },
+  ];
 
   return (
     <>
@@ -30,28 +60,38 @@ const CompanyInfo: FC = () => {
       <Row gutter={16} style={{ padding: "30px" }}>
         <Col xs={24} md={16}>
           <Card title="Company Details" bordered={false}>
-            <p style={{ fontSize: '20px' }}>Name: {company?.name}</p>
-            <p style={{ fontSize: '20px' }}>Email: {company?.mail}</p>
-            <p style={{ fontSize: '20px' }}>Phone: {company?.phoneNum}</p>
-            <p style={{ fontSize: '20px' }}>Description: {company?.description}</p>
-            <p style={{ fontSize: '20px' }}>Sector: {company?.sector}</p>
-            <p style={{ fontSize: '20px' }}>Country: {company?.country}</p>
+            <p style={{ fontSize: "20px" }}>Name: {company?.name}</p>
+            <p style={{ fontSize: "20px" }}>Email: {company?.mail}</p>
+            <p style={{ fontSize: "20px" }}>Phone: {company?.phoneNum}</p>
+            <p style={{ fontSize: "20px" }}>
+              Description: {company?.description}
+            </p>
+            <p style={{ fontSize: "20px" }}>Sector: {company?.sector}</p>
+            <p style={{ fontSize: "20px" }}>Country: {company?.country}</p>
           </Card>
         </Col>
         <Col xs={24} md={8}>
-          <img 
+          <img
             src={imagecompany}
-            alt="Company" 
-            style={{ 
-              width: '100%', 
-              height: 'auto', 
-              borderRadius: '20%'  
-            }} 
+            alt="Company"
+            style={{
+              width: "100%",
+              height: "auto",
+              borderRadius: "20%",
+            }}
           />
         </Col>
       </Row>
+      <Row gutter={16} style={{ padding: "30px" }}>
+        <Col xs={24}>
+          <span style={{ fontSize: "24px" }}>Active Company Agents </span>
+        </Col>
+      </Row>
+      <Row gutter={16} style={{ padding: "30px" }}>
+        <Table columns={columns} dataSource={companyagent} />
+      </Row>
     </>
   );
-          };  
+};
 
 export default CompanyInfo;
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Layout, Row, } from "antd";
+import { Col, Layout, Row } from "antd";
 import "./landing.css";
 import LandingNavbar from "./landing-navbar";
 import "./landing.css";
@@ -8,22 +8,28 @@ const { Content } = Layout;
 
 const LandingPage: React.FC = () => {
   return (
-    <Layout style={{ margin: "0px", padding: "0px" }} className="landing-background">
+    <Layout
+      style={{ margin: "0px", padding: "0px" }}
+      className="landing-background"
+    >
       <LandingNavbar />
       <Content style={{ padding: "200px" }}>
-        <Row justify={"center"} align={"top"} style={{ height: "100vh", width: "100%" }}>
+        <Row
+          justify={"center"}
+          align={"top"}
+          style={{ height: "100vh", width: "100%" }}
+        >
           <Col xs={24}>
             <h1 className="landing-text">
-              This platform is a free platform where people can express their opinions about companies without fear and companies can respond to criticism.!
+              This platform is a free platform where people can express their
+              opinions about companies without fear and companies can respond to
+              criticism.!
             </h1>
           </Col>
         </Row>
       </Content>
     </Layout>
   );
-}
-
-
-  
+};
 
 export default LandingPage;
